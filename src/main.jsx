cdb9b81b2f13bd123cbfb3e5ea001a9d98fa4dd1
Gsr99 <ganeshsreddy1@gmail.com>
2026-04-21 15:23:10 +0200

    Update Firebase hosting cache and refine project configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-// main.jsx
-import React from "react"
-import ReactDOM from "react-dom/client"
-import FlowHub from "./App.jsx"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FlowHub from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <FlowHub />
   </React.StrictMode>
-)
+);
